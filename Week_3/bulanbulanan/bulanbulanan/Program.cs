63fc742fb1b374172a1daa1c0db5ab1128190e23
unknown <hazimbitw@gmail.com>
2022-03-11 15:41:16 +0700

    tambah status mahasiswa

---
+++ Week_3/bulanbulanan/bulanbulanan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bulanbulanan
+{
+    class Program
+    {
+        public enum JenisKelamin { Pria = 0, Lakilaki, Wanita = 50, Perempuan, Lainlain = 99 };
+
+        static void Main(string[] args)
+        {
+            string[] contohArray = { "a", "b", "c", "d", "e", "f" };
+
+            Console.WriteLine(contohArray[0]);
+
+
+
+            StatusMahasiswa objStatusMhs = new StatusMahasiswa();
+            Console.WriteLine(objStatusMhs.StateSaatIni);
+
+            objStatusMhs.AksiYangDilakukan(StatusMahasiswa.Aksi.Lulus);
+            objStatusMhs.AksiYangDilakukan(StatusMahasiswa.Aksi.AjukanCuti);
+            objStatusMhs.AksiYangDilakukan(StatusMahasiswa.Aksi.SelesaiCuti);
+            objStatusMhs.AksiYangDilakukan(StatusMahasiswa.Aksi.CetakKSM);
+            objStatusMhs.AksiYangDilakukan(StatusMahasiswa.Aksi.Lulus);
+        }
+    }
+}
---
+++ Week_3/bulanbulanan/bulanbulanan/StatusMahasiswa.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bulanbulanan
+{
+    public class StatusMahasiswa
+    {
+        public enum Status
+        {
+            Terdaftar,
+            Aktif,
+            Lulus,
+            Cuti
+        };
+
+        public enum Aksi
+        {
+            CetakKSM,
+            Lulus,
+            AjukanCuti,
+            SelesaiCuti,
+        };
+
+        public class Transisi
+        {
+            public Status StateAwal;
+            public Status StateAkhir;
+            public Aksi Trigger;
+
+            public Transisi(Status awal, Status akhir, Aksi yangDilakukan)
+            {
+                StateAwal = awal;
+                StateAkhir = akhir;
+                Trigger = yangDilakukan;
+            }
+        }
+        Transisi[] listPerpindahanState =
+            {
+            new Transisi(Status.Terdaftar, Status.Aktif, Aksi.CetakKSM),
+            new Transisi(Status.Aktif, Status.Lulus, Aksi.Lulus),
+            new Transisi(Status.Terdaftar, Status.Cuti, Aksi.AjukanCuti),
+            new Transisi(Status.Cuti, Status.Terdaftar, Aksi.SelesaiCuti)
+        };
+
+        public Status StateSaatIni = Status.Terdaftar;
+
+        public Status GetStatusAkhir(Status awal, Aksi yangDilakukan)
+        {
+            Status statusAkhir = awal;
+
+            for (int i = 0; i < listPerpindahanState.Length; i++)
+            {
+                Status stateAwal = listPerpindahanState[i].StateAwal;
+                Aksi triggerState = listPerpindahanState[i].Trigger;
+
+                if (stateAwal == awal && triggerState == yangDilakukan)
+                {
+                    statusAkhir = listPerpindahanState[i].StateAkhir;
+                }
+            }
+
+            return statusAkhir;
+        }
+
+        public void AksiYangDilakukan(Aksi yangDilakukan)
+        {
+            Status stateBerikutnya = GetStatusAkhir(StateSaatIni, yangDilakukan);
+            StateSaatIni = stateBerikutnya;
+
+            Console.WriteLine("State saat ini adalah: " + StateSaatIni);
+
+            if (StateSaatIni == Status.Lulus)
+            {
+                Console.WriteLine("Selamat Anda Telah Lulus!");
+            }
+        }
+
+        public StatusMahasiswa()
+        {
+            StateSaatIni = Status.Terdaftar;
+        }
+    }
+}
